d79638419ff96a9032c414bc94d283a1f22e6362
TheBSquare <minka.denis@gmail.com>
2021-05-19 23:13:43 +0300

    Second commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+bot_token = 'your telegram bot token'
+
+db_host = 'host where you connect to your db'
+
+db_users_table = 'name of the table where all users will be saved'
+
+db_name = 'the name of your db'
+
+db_username = 'the username to connect to mysql'
+db_password = 'the password to connect to mysql'
